4c2710a83aa556b7dab578b6585979724621f4bc
James <jamespatwilson95@gmail.com>
2023-12-13 13:06:49 +0000

    Refactoring
    Refactoring main method

--- src/main/java/com/cme/palindrome/impl/service/PalindromeImplService.java
+++ src/main/java/com/cme/palindrome/impl/service/PalindromeImplService.java
@@ -17,25 +17,29 @@ public class PalindromeImplService implements PalindromeImpl {
 
         @Override
         public boolean palindromeChecker(String originalString) {
-                StringBuilder reverseValue = new StringBuilder(originalString.toLowerCase()).reverse();
-                boolean isPalindromeCheck = originalString.contentEquals(reverseValue);
+                loadPermanentCache();
 
-                        loadPermanentCache();
-                        if(!isValidString(originalString)) {
-                                log.error("String does not meet the correct format");
-                        }else{
-                                if(palindromeCache.isPalindromeCached(originalString)){
-                                        log.info("Input is a palindrome and was in the cache: " + originalString);
-                                }else{
-                                        if(isPalindromeCheck){
-                                                palindromeCache.addToCache(originalString);
-                                                log.info("Added string " + originalString + " to the cache");
-                                        }else {
-                                                log.error("Not a Palindrome: " + originalString);
-                                        }
-                                }
+                if (!isValidString(originalString)) {
+                        log.error("String does not meet the correct format");
+                        return false;
+                }
+
+                String lowerCaseString = originalString.toLowerCase();
+                String reverseValue = new StringBuilder(lowerCaseString).reverse().toString();
+                boolean isPalindromeCheck = lowerCaseString.equals(reverseValue);
+
+                if (palindromeCache.isPalindromeCached(originalString)) {
+                        log.info("Input is a palindrome and was in the cache: " + originalString);
+                } else {
+                        if (isPalindromeCheck && !palindromeCache.isPalindromeCached(originalString)) {
+                                palindromeCache.addToCache(originalString);
+                                log.info("Added string " + originalString + " to the cache");
+                        } else {
+                                log.error("Not a Palindrome: " + originalString);
                         }
-                        return isPalindromeCheck;
+                }
+
+                return isPalindromeCheck;
         }
 
         @Override
